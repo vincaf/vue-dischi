8f37de3ce8ae7794c89838d2bd93aef44d607306
vincaf <vincenzocafassog@gmail.com>
2022-07-19 17:03:07 +0200

    created header and main components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderWeb />
+    <MainWeb />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderWeb from './components/HeaderWeb.vue';
+import MainWeb from './components/MainWeb.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderWeb,
+    MainWeb,
   }
 }
 </script>
