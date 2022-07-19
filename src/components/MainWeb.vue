8f338b5eb07f2421967924d2ee4a0cb266494a6d
vincaf <vincenzocafassog@gmail.com>
2022-07-19 17:47:05 +0200

    added static layout MainWeb

--- src/components/MainWeb.vue
+++ src/components/MainWeb.vue
@@ -1,7 +1,120 @@
 <template>
-  <main>
-    
-  </main>
+    <main>
+        <div class="container">
+            <div class="cards-container d-flex align-content-stretch flex-wrap justify-content-between">
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.discogs.com/psDUUalbwHXuzNS7QgK93aG6ImE4JzALnOkCPtB1HVw/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNDY5/NTMtMTQ0ODkyNzA4/Ni02NTkwLmpwZWc.jpeg" class="card-img-top p-3" alt="">
+                    <div class="card-body text-center">
+                        <h5 class="text-white fw-bold">LIVE AT WEMBLEY 86</h5>
+                        <div class="card-text d-flex flex-column">
+                            <div>Queen</div>
+                            <div>1992</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -11,5 +124,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main{
+    background-color: #1e2d3b;
 
+    .container{
+        max-width: 1000px;
+
+        .cards-container{
+            padding: 50px 0;
+        }
+
+        .card{
+            width: 180px;
+            margin-bottom: 20px;
+            background-color: #2e3a46;
+            
+            .card-body{
+                padding: 0 10px 10px 10px;
+            }
+            .card-text{
+                color: #747a7c;
+                font-size: 14px;
+            }
+        }
+    }
+}
 </style>
